f3a2c0789d71c8374d7c479dbb9061b6a9b6e1a6
Ana Geguchadze <anageguchadze22@gmail.com>
2024-11-11 13:48:07 +0100

    fixed authentication bug

--- deliveries/views.py
+++ deliveries/views.py
@@ -1,11 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import *
 from .models import *
+from rest_framework.response import Response
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ParcelViewSet(viewsets.ModelViewSet):
     queryset = Parcel.objects.all()
